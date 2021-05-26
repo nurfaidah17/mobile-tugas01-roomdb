2c1328c5f51a96008098f62367050219daa805bd
nurfaidah17 <nurfaidahnuhung17@yahoo.com>
2021-05-26 21:01:07 +0800

    menambahkan class NoteViewModel.kt

---
+++ app/src/main/java/id/ac/nurfaidah/belajarroom/ui/main/NoteViewModel.kt
@@ -0,0 +1,30 @@
+package id.ac.nurfaidah.belajarroom.ui.main
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import id.ac.nurfaidah.belajarroom.db.note.Note
+import id.ac.nurfaidah.belajarroom.db.note.NoteRepository
+
+class NoteViewModel(application: Application) : AndroidViewModel(application) {
+
+    private var noteRepository = NoteRepository(application)
+    private var notes: LiveData<List<Note>>? = noteRepository.getNotes()
+
+    fun insertNote(note: Note) {
+        noteRepository.insert(note)
+    }
+
+    fun getNotes(): LiveData<List<Note>>? {
+        return notes
+    }
+
+    fun deleteNote(note: Note) {
+        noteRepository.delete(note)
+    }
+
+    fun updateNote(note: Note) {
+        noteRepository.update(note)
+    }
+    
+}
